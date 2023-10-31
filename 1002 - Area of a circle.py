bdd6edaa3361cffdd157b82433019586e57b6fe6
Lucas Alexandre <133163645+lucasalex7@users.noreply.github.com>
2023-10-31 15:52:20 -0300

    Add files via upload

---
+++ 1002 - Area of a circle.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+R = float(56.767)
+
+formula = 3.14159 * (R**2)
+
+print("A=%0.4f" %formula) 
---
+++ 1004 - Simple product.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+a = int(input())
+b = int(input())
+
+mult = (a) * (b)
+
+print("PROD =", mult)
