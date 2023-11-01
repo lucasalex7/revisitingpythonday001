f75d917d63a7c1773554f7c774e9403b87edf26c
Lucas Alexandre <133163645+lucasalex7@users.noreply.github.com>
2023-11-01 14:52:14 -0300

    Add files via upload

---
+++ 1008 - Salary.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+number = int(input())
+hours = int(input())
+hourate = float(input())
+
+salary = hours * hourate
+
+print("NUMBER =", number)
+print("SALARY = U$ %0.2f" %salary)
---
+++ 1009 - Salary with bonus.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+nome = input()
+salary = float(input())
+value = float(input())
+
+valor = salary + ((value/100) * 15)
+
+print("TOTAL = R$ %0.2f" %valor)
